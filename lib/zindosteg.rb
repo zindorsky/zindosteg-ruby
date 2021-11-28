51402283e60facba3645ab020ad899fd03567f20
Nephi Allred <nephi.allred@mx.com>
2021-11-28 13:02:15 -0700

    Alias 'new' to 'open'. 'insert' and 'extract' convenience functions.

--- lib/zindosteg.rb
+++ lib/zindosteg.rb
@@ -1,2 +1,18 @@
 require "zindosteg/version"
 require "zindosteg/zindosteg"
+
+module Zindosteg
+  class File
+    class << self
+      alias_method :open, :new
+    end
+  end
+
+  def self.insert(carrier, password, payload)
+    ::Zindosteg::File.open(carrier, password, "w").write(::File.open(payload).read)
+  end
+
+  def self.extract(carrier, password, payload)
+    ::File.open(payload, "w").write(::Zindosteg::File.open(carrier, password).read)
+  end
+end
